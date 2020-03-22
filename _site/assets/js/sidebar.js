aa18994100034e8fbe42238bff480c4635c61e22
Audun L. Solemdal <auls@equinor.com>
2020-03-22 13:10:07 +0100

    Move #main on sidebar toggle

--- _site/assets/js/sidebar.js
+++ _site/assets/js/sidebar.js
@@ -1,14 +1,11 @@
 function toggleHeader(){
     document.getElementById('header').classList.toggle('active');
-
-    const elements = document.getElementsByClassName("liquid");
-    const permanent = document.getElementsByClassName("liquid");
+    document.getElementById('main').classList.toggle('active');
 
     let check = document.getElementById('header').classList;
 
     if (check.contains("active"))
     {
-        // while (elements.length > 0) elements[0].remove();
         $("#collapse").children().hide();
         $(".copyright").children().hide();
     }
